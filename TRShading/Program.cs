d726dc878ddbba83091fb61794f2ffdcf2cc6a2c
Az <thomas.guyamier+Kraken@gmail.com>
2020-10-12 08:29:54 +0200

    Creation Projet

---
+++ TRShading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TRShading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
